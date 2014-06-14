2be9283e9d913036e93dc0c899a4cf3951473f1c
zzmp <zmp@umich.edu>
2014-06-14 01:45:52 -0700

    (refactor) Separated internal service decorators.

--- mod.js
+++ mod.js
@@ -52,46 +52,84 @@
     constant('eleven_', false).  // Debug         -services
     constant('eleven$', false).  // Debug         &services
     constant('eleven$$', false). // Debug        &&services (requires eleven&)
-    
+
     config(function($provide, $$elevenProvider,
                     eleven, eleven_, eleven$, eleven$$) {
+
       if (eleven) {
         // Decorate our services, conditionally.
         angular.forEach($$elevenProvider.services, function(serviceName) {
           // This can be improved with RegExp
-          if ((!eleven$$ && (serviceName.slice(0,2) === '$$')) ||
-              (!eleven$ && (serviceName[0] === '$')) ||
-              (!eleven_ && (serviceName[0] === '-'))) {
-            return;
-          }
-          $provide.decorator(serviceName, function($delegate, $log) {
-            var logger = function(key) {
-              var args = Array.prototype.slice.call(arguments, 1);
-              // This format is still up for discussion
-              // Eventually, it will log to a sidebar, outside of the console
-              $log.log('%c' + serviceName + '%c.%c' + key +
-                       '%c invoked with arguments:',
-                       'color: teal;',    // serviceName in teal
-                       'color: black;',
-                       'color: blue;',    // method in blue
-                       'color: black;');
-              $log.log('\t', args);
-              return $delegate[key].apply($delegate, args);
-            };
-
-            if (typeof $delegate === 'object') {
-              angular.forEach($delegate, function(val, key) {
-                window.service = $delegate;
-                logger[key] = (typeof val === 'function') ?
-                  logger.bind($delegate, key) :
-                  $delegate[key];
-              });
-
-              return logger;
+          if (serviceName[0] === '$') {
+            if ((!eleven$$ && (serviceName.slice(0,2) === '$$')) ||
+                (!eleven$ && (serviceName[0] === '$')) ||
+                serviceName === '$document' ||
+                serviceName === '$window' ||
+                serviceName === '$log') {
+              return;
             } else {
-              return $delegate;
+              $provide.decorator(serviceName, function($delegate, $log) {
+                var logger = function(key) {
+                  var args = Array.prototype.slice.call(arguments, 1);
+                  // This format is still up for discussion
+                  // Eventually, it will log to a sidebar, outside of the console
+                  $log.log('%c' + serviceName + '%c.%c' + key +
+                           '%c invoked with arguments:',
+                           'color: teal;',    // serviceName in teal
+                           'color: black;',
+                           'color: blue;',    // method in blue
+                           'color: black;');
+                  $log.log('\t', args);
+                  return $delegate[key].apply($delegate, args);
+                };
+
+                if (typeof $delegate === 'object') {
+                  angular.forEach($delegate, function(val, key) {
+                    window.service = $delegate;
+                    logger[key] = (typeof val === 'function') ?
+                      logger.bind($delegate, key) :
+                      $delegate[key];
+                  });
+
+                  return logger;
+                } else {
+                  return $delegate;
+                }
+              });
             }
-          });
+          } else {
+            if (!eleven_ && (serviceName[0] === '-')) {
+              return;
+            } else {
+              $provide.decorator(serviceName, function($delegate, $log) {
+                var logger = function(key) {
+                  var args = Array.prototype.slice.call(arguments, 1);
+                  // This format is still up for discussion
+                  // Eventually, it will log to a sidebar, outside of the console
+                  $log.log('%c' + serviceName + '%c.%c' + key +
+                           '%c invoked with arguments:',
+                           'color: teal;',    // serviceName in teal
+                           'color: black;',
+                           'color: blue;',    // method in blue
+                           'color: black;');
+                  $log.log('\t', args);
+                  return $delegate[key].apply($delegate, args);
+                };
+
+                if (typeof $delegate === 'object') {
+                  angular.forEach($delegate, function(val, key) {
+                    logger[key] = (typeof val === 'function') ?
+                      logger.bind($delegate, key) :
+                      $delegate[key];
+                  });
+
+                  return logger;
+                } else {
+                  return $delegate;
+                }
+              });
+            }
+          }
         });
       }
     })
